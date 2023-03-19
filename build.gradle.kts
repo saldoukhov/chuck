b6a6468b82bdd0a81343e4e76990bea74249cc98
Sergey Aldoukhov <saldoukhov@gmail.com>
2023-03-19 16:18:06 -0700

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("multiplatform") version "1.8.0"
+}
+
+val ktorVersion: String by project
+
+group = "us.yozik"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    sourceSets {
+        val nativeMain by getting {
+            dependencies {
+                implementation("com.aallam.openai:openai-client:3.1.1")
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+            }
+        }
+    }
+}
---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,86 @@
+import com.aallam.openai.api.BetaOpenAI
+import com.aallam.openai.api.chat.ChatCompletion
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.exception.OpenAIAPIException
+import com.aallam.openai.api.logging.LogLevel
+import com.aallam.openai.api.model.ModelId
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.client.OpenAIConfig
+import kotlinx.cinterop.toKString
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import platform.posix.fflush
+import platform.posix.fputs
+import platform.posix.getenv
+import platform.posix.stdout
+import kotlin.system.exitProcess
+
+@OptIn(BetaOpenAI::class)
+fun main(args: Array<String>) {
+    val apiKey = getenv("OPENAI_API_KEY")?.toKString()
+    val token = requireNotNull(apiKey) { "OPENAI_API_KEY environment variable must be set." }
+    val parser = ArgParser("./chuck.kexe %Model Id%")
+    val model by parser.argument(ArgType.String, description = "Model Id to use (gpt-4, gpt-3.5-turbo, etc.)")
+    parser.parse(args)
+    println("Hello, I'm Chuck, your OpenAI assistant")
+    runBlocking {
+        val openAI = OpenAI(OpenAIConfig(token, LogLevel.None))
+        val conversation = mutableListOf<ChatMessage>()
+        while (true) {
+            print("🤖 ")
+            val input = readln()
+            if (input == "exit") {
+                break
+            }
+            conversation.add(
+                ChatMessage(
+                role = ChatRole.User,
+                content = input
+            ))
+            val progress = launch {
+                while (true) {
+                    print("💬")
+                    delay(2000)
+                }
+            }
+            hideCursor()
+            try {
+                val chatCompletionRequest = ChatCompletionRequest(
+                    model = ModelId(model),
+                    messages = conversation
+                )
+                val completion: ChatCompletion = openAI.chatCompletion(chatCompletionRequest)
+                val response = completion.choices[0].message?.content ?: break
+                println(response)
+                conversation.add(
+                    ChatMessage(
+                    role = ChatRole.Assistant,
+                    content = response
+                ))
+                progress.cancelAndJoin()
+                showCursor()
+            } catch (e: Exception) {
+                val openAIAPIException = e.cause as? OpenAIAPIException
+                println(openAIAPIException ?: e.message)
+                break
+            }
+        }
+    }
+    exitProcess(0)
+}
+
+fun hideCursor() {
+    fputs("\u001b[?25l", stdout)
+    fflush(stdout)
+}
+
+fun showCursor() {
+    fputs("\u001b[?25h", stdout)
+    fflush(stdout)
+}
